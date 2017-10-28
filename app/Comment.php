180fac0a7605d314df2848a2d6ee2221e0c6e7c1
sushuai <sushuai@didichuxing.com>
2017-10-28 17:04:13 +0800

    读取评论以及删除评论的api

--- app/Comment.php
+++ app/Comment.php
@@ -60,11 +60,49 @@ class Comment extends Model
 
     public function read()
     {
+        if (!rq('question_id') && !rq('answer_id')){
+            return ['status' => 0, 'msg' => 'question_id or answer_id is required'];
+        }
+        if (rq('question_id')) {
+          $question = question_ins()->find(rq('question_id'));
+          if(!$question)  return ['status' => 0, 'msg' => 'question not exists'];
+            $data = $this->where('question_id',rq('question_id'))->get();
+        }
+        else {
+            $answer = question_ins()->find(rq('answer_id'));
+            if(!$answer)  return ['status' => 0, 'msg' => 'answer not exists'];
+            $data = $this->where('answer_id',rq('answer_id'))->get();
+        }
 
+        return ['status' => 1, 'data' => $data->keyBy('id')];
     }
 
+    //删除评论的api
     public function remove()
     {
+        //检查用户是否登录
+        if (!user_ins()->is_logined_in()) {
+            return ['status' => 0, 'msg' => 'login required'];
+        }
+
+        if (!rq('id')) {
+            return ['status' => 0, 'msg' => 'id is required'];
+        }
+
+        $comment = $this->find(rq('id'));
+        if (!$comment) {
+            return ['status' => 0, 'msg' => 'comment not exists'];
+        }
+
+        if (session('user_id') != $comment->user_id) {
+            return ['status' => 0, 'msg' => 'permission denied'];
+        }
+
+        //删除回复该评论的评论
+        $this->where('reply_to', rq('id'))->delete();
+        //再删除该评论
+        return $comment->delete() ? ['status' => 1]
+            : ['status' => 0, 'msg' => 'db delete failed'];
 
     }
 }
