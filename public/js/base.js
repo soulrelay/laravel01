920fc4b4568fc9c21b18ae30aa6823747a0cc1d1
sushuai <sushuai@didichuxing.com>
2017-10-29 22:43:32 +0800

    表单验证 13-3 05-24

--- public/js/base.js
+++ public/js/base.js
@@ -4,40 +4,44 @@
     angular.module('shuaihu', [
         'ui.router',
     ])
-        .config(function ($interpolateProvider,
-                          $stateProvider,
-                          $urlRouterProvider) {
+        .config(['$interpolateProvider', '$stateProvider', '$urlRouterProvider', function ($interpolateProvider,
+                                                                                           $stateProvider,
+                                                                                           $urlRouterProvider) {
             $interpolateProvider.startSymbol('[:');
             $interpolateProvider.endSymbol(':]');
 
             $urlRouterProvider.otherwise('/home');
 
             $stateProvider
-                .state('home',{
-                    url:'/home',
+                .state('home', {
+                    url: '/home',
                     templateUrl: 'home.tpl'
                 })
-                .state('login',{
-                    url:'/login',
+                .state('login', {
+                    url: '/login',
                     templateUrl: 'login.tpl'
 
                 })
-                .state('signup',{
-                    url:'/signup',
+                .state('signup', {
+                    url: '/signup',
                     templateUrl: 'signup.tpl'
 
                 })
 
-        })
+        }])
 
+        .service('UserService', [function () {
+            var me = this;
+            me.signup_data = {};
+            me.signup = function () {
 
+            }
+        }])
 
-        .controller('TestController', function ($scope) {
-            $scope.name = 'Test';
-        })
-        .controller('ParentController',function ($scope) {
-            $scope.name = 'Parent';
-        })
+        .controller('SignupController', ['$scope', 'UserService', function ($scope, UserService) {
+            $scope.User = UserService;
+        }])
+
 
     /**/
 })();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,21 +50,70 @@
 <script type="text/ng-template" id="home.tpl">
     <div class="home  container">
         首页
-        就看见乐视了dddddddddddddddddddddd的点点滴滴多多多多多多多多多多多多多多多多多多多多多的点点滴滴多多多多多多多多多多多的点点滴滴多多多多多多多多多多多多多多多多多多多多多
     </div>
 </script>
 
 <script type="text/ng-template" id="login.tpl">
     <div class="home  container">
         登录
-        就看见乐视了dddddddddddddddddddddd的点点滴滴多多多多多多多多多多多多多多多多多多多多多的点点滴滴多多多多多多多多多多多的点点滴滴多多多多多多多多多多多多多多多多多多多多多
     </div>
 </script>
 
 <script type="text/ng-template" id="signup.tpl">
-    <div class="home  container">
-        注册
-        就看见乐视了dddddddddddddddddddddd的点点滴滴多多多多多多多多多多多多多多多多多多多多多的点点滴滴多多多多多多多多多多多的点点滴滴多多多多多多多多多多多多多多多多多多多多多
+    <div ng-controller="SignupController" class="signup  container">
+        <div class="card">
+            <h1>注册</h1>
+            [: User.signup_data :]
+            <form name="signup_form" ng-submit="User.signup()">
+                <div class="input-group">
+                    <label>用户名：</label>
+                    <input name="username"
+                           type="text"
+                           ng-minlength="4"
+                           ng-maxlength="16"
+                           ng-model="User.signup_data.username"
+                           {{--ng-model-options="{updateOn: 'blur'}"--}}
+                           required
+                    >
+                    <div ng-if="signup_form.username.$touched" class="input-error-set">
+                        <div ng-if="signup_form.username.$error.required">
+                            用户名为必填项
+                        </div>
+                        <div ng-if="signup_form.username.$error.maxlength ||
+                        signup_form.username.$error.minlength"
+                        >用户名长度需在4至24之间
+                        </div>
+                    </div>
+
+                </div>
+                <div class="input-group">
+                    <label>密码：</label>
+                    <input name="password"
+                           type="password"
+                           ng-minlength="6"
+                           ng-maxlength="255"
+                           ng-model="User.signup_data.password"
+                           {{--ng-model-options="{updateOn: 'blur'}"--}}
+                           required
+                    >
+                    <div ng-if="signup_form.password.$touched" class="input-error-set">
+                        <div ng-if="signup_form.password.$error.required">
+                            密码为必填项
+                        </div>
+                        <div ng-if="signup_form.password.$error.maxlength ||
+                        signup_form.password.$error.minlength"
+                        >密码长度需在6至255之间
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit"
+                        ng-disabled="signup_form.$invalid"
+                >注册
+                </button>
+
+            </form>
+        </div>
     </div>
 </script>
 </html>
