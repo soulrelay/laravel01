6bf30cb028dd66d453cd1022fb42714e67ac66da
sushuai <sushuai@didichuxing.com>
2017-10-29 11:49:17 +0800

    angular基本概念

--- public/js/base.js
+++ public/js/base.js
@@ -1,1 +1,17 @@
-console.log(1);
+;(function () {
+    'use strict';
+
+    angular.module('shuaihu', [])
+        .config(function ($interpolateProvider) {
+            $interpolateProvider.startSymbol('[:');
+            $interpolateProvider.endSymbol(':]');
+        })
+        .controller('TestController', function ($scope) {
+            $scope.name = 'Test';
+        })
+        .controller('ParentController',function ($scope) {
+            $scope.name = 'Parent';
+        })
+
+    /**/
+})();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,37 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <!-- npm install angular jquery angular-ui-router normalize-css -->
-        <!--         百度静态资源公共库 http://cdn.code.baidu.com     稍微慢点 也可以手动下载添加 类似npm做的工作    -->
-        <!-- http://apps.bdimg.com/libs/angular.js/1.4.6/angular.js -->
-        <!-- Fonts -->
+<html lang="{{ app()->getLocale() }}" ng-app="shuaihu">
+<head>
+    <meta charset="utf-8">
+    <!-- npm install angular jquery angular-ui-router normalize-css -->
+    <!--         百度静态资源公共库 http://cdn.code.baidu.com     稍微慢点 也可以手动下载添加 类似npm做的工作    -->
+    <!-- http://apps.bdimg.com/libs/angular.js/1.4.6/angular.js -->
+    <!-- Fonts -->
 
 
+    <title>帅乎</title>
+    <link rel="stylesheet" href="node_modules/normalize-css/normalize.css">
+    <link rel="stylesheet" href="css/base.css">
 
-        <title>帅乎</title>
-        <link rel="stylesheet" href="node_modules/normalize-css/normalize.css">
-        <link rel="stylesheet" href="css/base.css">
-
-        <script src="node_modules/jquery/dist/jquery.js"></script>
-        <script src="node_modules/angular/angular.js"></script>
-        <script src="node_modules/angular-ui-router/release/angular-ui-router.js"></script>
-        <script src="js/base.js"></script>
-
+    <script src="node_modules/jquery/dist/jquery.js"></script>
+    <script src="node_modules/angular/angular.js"></script>
+    <script src="node_modules/angular-ui-router/release/angular-ui-router.js"></script>
+    <script src="js/base.js"></script>
 
 
-    </head>
-    <body>
-        帅乎
-    </body>
+</head>
+<body>
+帅乎
+<div ng-controller="ParentController">
+
+    <div ng-controller="TestController">
+        name1是[: name :]
+    </div>
+
+    <div>
+        name2是[: name :]
+    </div>
+
+</div>
+
+</body>
 </html>
